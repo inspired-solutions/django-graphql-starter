8ff9cdb5d242e01527f27cc37574aa3c316276a8
Sergio <helpse@gmail.com>
2018-06-26 15:01:06 -0500

    upd endpoints

--- core/schema.py
+++ core/schema.py
@@ -1,17 +1,26 @@
-# import graphene
-from graphene import Node
+import graphene
+# from graphene import Node
 from graphene_django.types import DjangoObjectType
-from graphene_django.filter import DjangoFilterConnectionField
-
+# from graphene_django.filter import DjangoFilterConnectionField
 
 from django.contrib.auth.models import *
+
+
+class UserWhereUniqueInput(graphene.InputObjectType):
+    id = graphene.Int()
+
+
+class UserWhereInput(UserWhereUniqueInput):
+    AND = graphene.Field('core.schema.UserWhereInput')
+    OR = graphene.Field('core.schema.UserWhereInput')
+    email = graphene.String()
 
 
 class UserNode(DjangoObjectType):
     class Meta:
         model = User
-        filter_fields = ['email', ]
-        interfaces = (Node, )
+        # filter_fields = ['email', ]
+        # interfaces = (Node, )
 
 
 class GroupNode(DjangoObjectType):
@@ -28,12 +37,35 @@ class PermissionNode(DjangoObjectType):
         # interfaces = (Node, )
 
 
-class Query(object):
-    # all_users = graphene.List(UserNode)
-    all_users = DjangoFilterConnectionField(UserNode)
+def user_filter(queryset, where):
+    if where is None:
+        return queryset
 
-    '''def resolve_all_users(self, info, **kwargs):
-        return User.objects.all()'''
+    if where.get('AND'):
+        queryset = user_filter(queryset, where.get('AND'))
+
+    if where.get('OR'):
+        queryset = queryset + user_filter(queryset, where.get('OR'))
+
+    if where.get('email'):
+        queryset = queryset.filter(email=where.get('email'))
+
+    if where.get('id'):
+        queryset = queryset.filter(id=where.get('id'))
+
+    return queryset
+
+
+class Query(object):
+    all_users = graphene.List(UserNode, where=UserWhereInput(required=False))
+    user = graphene.Field(UserNode, where=UserWhereUniqueInput(required=True))
+    # all_users = DjangoFilterConnectionField(UserNode)
+
+    def resolve_all_users(self, info, where=None):
+        return user_filter(User.objects.all(), where)
+
+    def resolve_user(self, info, where):
+        return user_filter(User.objects.all(), where).first()
 
 
 class Mutation(object):
--- djangographql/urls.py
+++ djangographql/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-# from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.csrf import csrf_exempt
 from graphene_django.views import GraphQLView
 
 # from .middleware import TokenAuthenticationMiddleware
@@ -24,5 +24,6 @@ from graphene_django.views import GraphQLView
 urlpatterns = [
     path('admin/', admin.site.urls),
     # path('graphql/', csrf_exempt(GraphQLView.as_view(graphiql=True, middleware=[TokenAuthenticationMiddleware]))),
-    path('graphql/', GraphQLView.as_view(graphiql=True)),
+    # path('graphql/', GraphQLView.as_view(graphiql=True)),
+    path('graphql/', csrf_exempt(GraphQLView.as_view())),
 ]
