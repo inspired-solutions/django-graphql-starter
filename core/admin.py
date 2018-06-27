a6f5b7790d47047a99c8f66f5afaf08b99a671eb
Sergio Melendez <hlpse@hotmail.com>
2018-06-27 09:51:39 -0500

    fix merge conflicts

--- core/admin.py
+++ core/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-# from .models import Permission
+from django.contrib.auth.models import Permission
 
 # Register your models here.
-# admin.site.register(Permission)
+admin.site.register(Permission)
