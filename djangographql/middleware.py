a53c888354079c72f6134948265eb04324b87044
Sergio <helpse@gmail.com>
2018-07-18 17:23:49 -0500

    added introspection token exempt

--- djangographql/middleware.py
+++ djangographql/middleware.py
@@ -3,14 +3,23 @@ from rest_framework.authentication import TokenAuthentication
 
 class TokenAuthenticationMiddleware(object):
     def resolve(self, next, root, info, **args):
-        token_exempt = getattr(next.args[0], 'token_exempt', False)
 
-        if root is None and not token_exempt:
-            result = TokenAuthentication().authenticate(info.context)
+        if root is None:
+            token_exempt = getattr(next.args[0], 'token_exempt', False)
+            is_introspection = getattr(info.operation.name, 'value', False) == 'IntrospectionQuery'
 
-            assert result is not None, 'Authentication credentials were not provided.'
+            if not token_exempt and not is_introspection:
+                result = TokenAuthentication().authenticate(info.context)
 
-            user, token = result
+                assert result is not None, 'Authentication credentials were not provided.'
 
-            info.context.user = user
+                user, token = result
+
+                info.context.user = user
+
+        return next(root, info, **args)
+
+
+class ACLMiddleware(object):
+    def resolve(self, next, root, info, **args):
         return next(root, info, **args)
