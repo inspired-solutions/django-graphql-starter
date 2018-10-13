d03873e99cc07396b360221f7794849d40399d07
Sergio <helpse@gmail.com>
2018-10-13 02:46:58 -0500

    remove unchanged method

--- opencrud/custom.py
+++ opencrud/custom.py
@@ -2,11 +2,8 @@ import graphene
 
 from collections import OrderedDict
 from graphene_django.filter import DjangoFilterConnectionField
-from graphene_django.utils import maybe_queryset
 from graphene.types.argument import to_arguments
-from graphene.relay import PageInfo
 
-from django.db.models.query import QuerySet
 from .utils import get_filterset_class, get_filtering_args_from_filterset, connection_from_list_slice, \
     get_where_input_field, get_connection_class
 
@@ -68,32 +65,6 @@ class CustomDjangoFilterConnectionField(DjangoFilterConnectionField):
     @property
     def filtering_args(self):
         return get_filtering_args_from_filterset(self.filterset_class, self.node_type)
-
-    @classmethod
-    def resolve_connection(cls, connection, default_manager, args, iterable):
-        if iterable is None:
-            iterable = default_manager
-        iterable = maybe_queryset(iterable)
-        if isinstance(iterable, QuerySet):
-            if iterable is not default_manager:
-                default_queryset = maybe_queryset(default_manager)
-                iterable = cls.merge_querysets(default_queryset, iterable)
-            _len = iterable.count()
-        else:
-            _len = len(iterable)
-        connection = connection_from_list_slice(
-            iterable,
-            args,
-            slice_start=0,
-            list_length=_len,
-            list_slice_length=_len,
-            connection_type=connection,
-            edge_type=connection.Edge,
-            pageinfo_type=PageInfo,
-        )
-        connection.iterable = iterable
-        connection.length = _len
-        return connection
 
     @classmethod
     def connection_resolver(
